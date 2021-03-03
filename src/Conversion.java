9964ef4db8ccf4be94c11d5b07b565fe9869aab1
GitElean <eleanjulian@gmail.com>
2021-03-02 19:05:48 -0600

    02/03/21
    Intfix to Postfix casi terminado, juntit test

--- src/Conversion.java
+++ src/Conversion.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /***
  * Clase encargada de realizar la transformaicón de notacion intfix a postfix
  */
@@ -6,28 +8,106 @@
 public class Conversion{
     ReaderTxt rTxt = new ReaderTxt();
     SimpleChain sc = new SimpleChain();
-
+    private String iS;
+    private Stack s;
     private String file1;
-    private String file2;
-
+/*
     public String intToPost(char charArray[]){
 
         for (int i=0; i>charArray.length;i++){
+            if (isOrder(charArray[i])==true){
+                charArray[i]=' ';
+            }else if(isOp(charArray[i])){
 
+            }else if (isNum(charArray[i])){
+
+            }
         }
 
         String str = String.valueOf(charArray);
         return str;
     }
 
-    private void cadena(){
-        char[] start={};
+    private void lista(char charArray[]){
+
         char[] stack={};
         char[] exit={};
-        sc.addFirst(start);
+        sc.addFirst(charArray);
         sc.addLast(stack);
         sc.addLast(exit);
     }
+    private void makeChar(String file1){
+        char[] charArray=file1.toCharArray();
+    }
+*/
+
+    public String PostfixEval(String file1){
+        iS = file1;
+        s = new Stack();
+        String outputString = "";
+        boolean fI = false;
+        for(int i = 0;i < iS.length();i++){
+            char curChar = iS.charAt(i);
+            if (isOrd(curChar)){
+                curChar=' ';
+            }
+            if(!isOp(curChar)){
+                outputString += Character.toString(curChar);
+                if(i == (iS.length()-1)){
+                    while(!s.empty()){
+                        outputString += s.pop();
+                    }
+                }
+            }else{
+                if(fI){
+                    if(pMin(curChar) && pMin((Character)s.peek())){
+                        outputString += s.pop();
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else if(mDiv(curChar) && mDiv((Character)s.peek())){
+                        outputString += s.pop();
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else if(pMin(curChar) && mDiv((Character)s.peek())){
+                        outputString += s.pop();
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else if(mDiv(curChar) && pMin((Character)s.peek())){
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else{
+                        outputString += Character.toString(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }
+                }else{
+                    s.push(curChar);
+                    fI = true;
+                }
+            }
+        }
+        return outputString;
+    }
+
 
     public String getFile1() {
         return file1;
@@ -37,19 +117,7 @@ public class Conversion{
         this.file1 = file1;
     }
 
-    public String getFile2() { return file2; }
 
-    public void setFile2(String file2) {
-        this.file2 = file2;
-    }
-
-    private void makeChar(String file1){
-        char[] charArray=file1.toCharArray();
-    }
-
-    private void makeChar2(String file2){
-        char[] charArray=file2.toCharArray();
-    }
 
     private boolean isOp(char op){
         return switch (op) {
@@ -61,8 +129,23 @@ public class Conversion{
             default -> false;
         };
     }
+    private boolean pMin(char op){
+        return switch (op){
+          case '+' -> true;
+          case '-' -> true;
+          default -> false;
+        };
+    }
 
-    private boolean isOrder(char op){
+    private boolean mDiv(char op){
+        return switch (op){
+            case '*' -> true;
+            case '/' -> true;
+            default -> false;
+        };
+    }
+
+    private boolean isOrd(char op){
         return switch (op){
             case '(' -> true;
             case ')' -> true;
@@ -70,19 +153,4 @@ public class Conversion{
         };
     }
 
-    private boolean isNum(char op){
-        return switch (op){
-            case '0' -> true;
-            case '1' -> true;
-            case '2' -> true;
-            case '3' -> true;
-            case '4' -> true;
-            case '5' -> true;
-            case '6' -> true;
-            case '7' -> true;
-            case '8' -> true;
-            case '9' -> true;
-            default -> false;
-        };
-    }
 }
--- test/java/ConversionTest.java
+++ test/java/ConversionTest.java
@@ -1,7 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Random;
+import java.util.Stack;
+
 class ConversionTest {
     Random rand = new Random();
+    private Stack s;
     @org.junit.jupiter.api.Test
     void isOp() {
         char op='1';
@@ -22,5 +25,122 @@ class ConversionTest {
         for (char c:charArray){
             System.out.println(c);
         }
+    }
+
+    @org.junit.jupiter.api.Test
+    void PostfixEval(){
+        String iS = "(2+3)*2";
+        s = new Stack();
+        String outputString = "";
+        boolean fI = false;
+        for(int i = 0;i < iS.length();i++){
+            char curChar = iS.charAt(i);
+            if (isOrd(curChar)){
+                curChar=' ';
+            }
+            if(!isOp(curChar)){
+                outputString += Character.toString(curChar);
+                if(i == (iS.length()-1)){
+                    while(!s.empty()){
+                        outputString += s.pop();
+                    }
+                }
+            }else{
+                if(fI){
+                    if(pMin(curChar) && pMin((Character)s.peek())){
+                        outputString += s.pop();
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else if(mDiv(curChar) && mDiv((Character)s.peek())){
+                        outputString += s.pop();
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else if(pMin(curChar) && mDiv((Character)s.peek())){
+                        outputString += s.pop();
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else if(mDiv(curChar) && pMin((Character)s.peek())){
+                        s.push(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }else{
+                        outputString += Character.toString(curChar);
+                        if(i == (iS.length()-1)){
+                            while(!s.empty()){
+                                outputString += s.pop();
+                            }
+                        }
+                    }
+                }else{
+                    s.push(curChar);
+                    fI = true;
+                }
+            }
+        }
+        System.out.println(outputString);
+    }
+
+
+
+    private boolean isOp(char op){
+        switch(op){
+            case '+':
+                return true;
+            case '-':
+                return true;
+            case '/':
+                return true;
+            case '*':
+                return true;
+            case '^':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private boolean pMin(char op){
+        switch(op){
+            case '+':
+                return true;
+            case '-':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private boolean mDiv(char op){
+        switch(op){
+            case '*':
+                return true;
+            case '/':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private boolean isOrd(char op){
+        return switch (op){
+            case '(' -> true;
+            case ')' -> true;
+            default -> false;
+        };
     }
 }
